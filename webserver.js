f40ad1cfb9f49d4ff03769a7ba09b671fc8093da
senyoltw <senyoltw@users.noreply.github.com>
2017-01-04 07:47:31 +0900

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,5 +1,20 @@
 var http = require('http');
+var spawn = require('child_process').spawn;
+var app = spawn('node', ['app.js']);
+
+app.stdout.on('data', function (data) {
+  console.log('stdout: ' + data);
+});
+
+app.stderr.on('data', function (data) {
+  console.log('stderr: ' + data);
+});
+
+app.on('close', function (code) {
+  console.log('child process exited with code ' + code);
+});
+
 http.createServer(function(req,res){
 res.writeHead(200,{'Content-Type': 'text/plain'});
-res.end('Hello, world');
+res.end('egosearch start!');
 }).listen(80,'0.0.0.0');
