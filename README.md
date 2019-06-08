ce20f6f3fe725da35a08b3cb8be2e9564feaa74d
senyoltw <senyoltw@users.noreply.github.com>
2019-06-09 02:13:36 +0900

    change use statuses/home_timeline

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-# docker-egosearch
-Twitterのタイムラインをエゴサしてpushbulletを使って通知する dockerコンテナ
+# twitter-egosearch
+TwitterのタイムラインをエゴサしてIFTTTのnotificationを使って通知する dockerコンテナ
 
 # 使い方
 apps.twitter.comからConsumer Key / Consumer Secret / Access Token / Access Token Secret を取得  
 https://apps.twitter.com/
 
-PushBulletのアカウントを作り、Account SettingsからAccess Tokenを取得  
-https://www.pushbullet.com/
+IFTTTのアカウントを作り、webhookとnotificationをつなげる。 
+https://ifttt.com/maker_webhooks
+https://ifttt.com/if_notifications
 
 docker環境があるところで以下コマンドで実行
 ```
 # docker pull senyoltw/twitter_egosearch
 # docker run \
 -e keyword="エゴサしたい単語|正規表現も大丈夫" \
--e PushBullet_device='プッシュしたいデバイス' \
--e PushBullet_Access_Token='取得したやつ' \
+-e webhook_url='webhookのURL' \
 -e twitter_consumer_key='取得したやつ' \
 -e twitter_consumer_secret='取得したやつ' \
 -e twitter_access_token_key='取得したやつ' \
--- app.js
+++ app.js
@@ -1,41 +1,72 @@
-// エラー処理は終了にしているのでforeverなどでプロセス監視してください
-var keyword = process.env.keyword
-var device =  process.env.PushBullet_device
-var PushBullet = require('pushbullet');
-var pusher = new PushBullet(process.env.PushBullet_Access_Token);
-var twitter = require('twitter');
-var twit = new twitter({
+// エラー処理は終了にしているので適当にプロセス監視してください
+const keyword = process.env.keyword
+const axios = require('axios');
+const webhook_url = process.env.webhook_url
+const twitter = require('twitter');
+const client = new twitter({
   consumer_key: process.env.twitter_consumer_key,
   consumer_secret: process.env.twitter_consumer_secret,
   access_token_key: process.env.twitter_access_token_key,
   access_token_secret: process.env.twitter_access_token_secret
 });
 
-pusher.note(device,'New Search', 'New Search Start' , function(error, response) {
-      if (error) {
-        console.log('pushBullet disconnection error. exit.');
-        process.exit(1);
-      } else {
-        console.log('New Search Start');
-      }
-});
-
-twit.stream('user',  function(stream) {
-  stream.on('data', function (data) {
-    if (data && data.user && data.text.search(keyword)!=-1) {
-     pusher.link(device, data.user.name+"@"+data.user.screen_name+" : "+data.text, "https://twitter.com/"+data.user.screen_name+"/status/"+data.id_str, function(error, response) {
-      if (error) {
-        console.log('pushBullet disconnection error. exit.');
-        process.exit(1);
-      } else {
-        console.log(data.user.name+"@"+data.user.screen_name+" : "+data.text);
-      }
-     });
-    }
-  });
-  stream.on('error', function (response) {
-    // Handle a disconnection
-    console.log('twitter error. exit.');
+function post_webhook(data) {
+  axios.post(webhook_url, data)
+  .catch(function (error) {
+    console.log('webhook error.');
+    console.log(error);
     process.exit(1);
-  });
-});
+  })
+}
+
+function get_home_timeline (params) {
+  return client.get('statuses/home_timeline', params)
+  .then(function (tweets) {
+    return tweets;
+  })
+  .catch(function (error) {
+    console.log('twitter error.');
+    console.log(error);
+    process.exit(1);
+  })
+}
+
+function search_tweet (data, keyword) {
+  if (data.full_text.search(keyword)!=-1) {
+    let body = { value1 : data.user.name+"@"+data.user.screen_name, value2 : data.full_text, value3 : "https://twitter.com/"+data.user.screen_name+"/status/"+data.id_str };
+    return body;
+  }else{
+    return {};
+  }
+}
+
+async function main () {
+  console.log('twitter search start!');
+  let params = {count:1, tweet_mode:'extended'};
+  let timeline = await get_home_timeline(params)
+  let since_id = timeline[0].id_str
+  while (true){
+    // twitter api 15times/15min
+    await new Promise(r => setTimeout(r, 61000));
+    params = {count:200, since_id: since_id, tweet_mode:'extended'};
+    timeline = await get_home_timeline(params);
+    if (Object.keys(timeline).length) {
+      // There are new tweets.
+      Object.keys(timeline).forEach(function(key) {
+        let tweet = this[key];
+        let hit = search_tweet(tweet, keyword);
+        if (Object.keys(hit).length) {
+          console.log('hit!'+hit);
+          post_webhook(hit);
+        }
+      }, timeline);
+      // next search from since_id  (that is, more recent than) 
+      since_id = timeline[0].id_str
+    }else{
+      // There are no new tweets.
+      console.log('nodata');
+    }
+  }
+}
+
+main();
