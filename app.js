8afacfc93448c7dea25ad1cfd68ca7e4bf3b8725
senyoltw <senyoltw@users.noreply.github.com>
2019-06-09 15:05:16 +0900

    fix since_id. when no new tweets. error.

--- app.js
+++ app.js
@@ -12,48 +12,47 @@ const client = new twitter({
 
 function post_webhook(data) {
   axios.post(webhook_url, data)
-  .catch(function (error) {
-    console.log('webhook error.');
-    console.log(error);
-    process.exit(1);
-  })
+    .catch(function (error) {
+      console.log('webhook error.');
+      console.log(error);
+      process.exit(1);
+    })
 }
 
-function get_home_timeline (params) {
+function get_home_timeline(params) {
   return client.get('statuses/home_timeline', params)
-  .then(function (tweets) {
-    return tweets;
-  })
-  .catch(function (error) {
-    console.log('twitter error.');
-    console.log(error);
-    process.exit(1);
-  })
+    .then(function (tweets) {
+      return tweets;
+    })
+    .catch(function (error) {
+      console.log('twitter error.');
+      console.log(error);
+      process.exit(1);
+    })
 }
 
-function search_tweet (data, keyword) {
-  if (data.full_text.search(keyword)!=-1) {
-    let body = { value1 : data.user.name+"@"+data.user.screen_name, value2 : data.full_text, value3 : "https://twitter.com/"+data.user.screen_name+"/status/"+data.id_str };
+function search_tweet(data, keyword) {
+  if (data.full_text.search(keyword) != -1) {
+    let body = { value1: data.user.name + "@" + data.user.screen_name, value2: data.full_text, value3: "https://twitter.com/" + data.user.screen_name + "/status/" + data.id_str };
     return body;
-  }else{
+  } else {
     return {};
   }
 }
 
-async function main () {
+async function main() {
   console.log('twitter search start!');
-  let params = {count:1, tweet_mode:'extended'};
+  let params = { count: 1, tweet_mode: 'extended' };
   let timeline = await get_home_timeline(params)
-  while (true){
+  let since_id = timeline[0].id_str
+  while (true) {
     // twitter api 15times/15min
     await new Promise(r => setTimeout(r, 61000));
-    // next search from since_id  (that is, more recent than) 
-    let since_id = timeline[0].id_str
-    params = {count:200, since_id: since_id, tweet_mode:'extended'};
+    params = { count: 200, since_id: since_id, tweet_mode: 'extended' };
     timeline = await get_home_timeline(params);
     if (Object.keys(timeline).length) {
       // There are new tweets.
-      Object.keys(timeline).forEach(function(key) {
+      Object.keys(timeline).forEach(function (key) {
         // analysis new tweet. one by one.
         let tweet = this[key];
         let hit = search_tweet(tweet, keyword);
@@ -63,7 +62,9 @@ async function main () {
           post_webhook(hit);
         }
       }, timeline);
-    }else{
+      // next search from since_id  (that is, more recent than) 
+      since_id = timeline[0].id_str
+    } else {
       // There are no new tweets.
     }
   }
