3717af9567248d5b939d7d745e90abbb4c3e674c
senyoltw <senyoltw@users.noreply.github.com>
2020-03-31 01:49:56 +0900

    errlog

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const client = new twitter({
 function post_webhook(data) {
   axios.post(webhook_url, data)
     .catch(function (error) {
-      console.error('webhook error.', error);
+      console.error('webhook error : %err', error);
       process.exit(1);
     })
 }
@@ -24,7 +24,7 @@ function get_home_timeline(params) {
       return tweets;
     })
     .catch(function (error) {
-      console.error('twitter error.', error);
+      console.error('twitter error : %err', error);
       process.exit(1);
     })
 }
